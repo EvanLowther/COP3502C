108f601167c4ddb656aa0f33c88c7e2179caf066
EvanLowther <153966992+EvanLowther@users.noreply.github.com>
2024-09-03 15:09:38 -0400

    Lab1_C.py

--- Lab1_C.py
+++ Lab1_C.py
@@ -1,3 +1,5 @@
+#Calculates the days between two dates
+
 year1 = int(input("Enter the year for date 1: "))
 month1 = int(input("Enter the month for date 1: "))
 day1 = int(input("Enter the day for date 1: "))
