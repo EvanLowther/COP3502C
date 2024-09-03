dd8895371bff6f47f1416549bdb3b974ee93a854
EvanLowther <153966992+EvanLowther@users.noreply.github.com>
2024-09-03 15:08:50 -0400

    Lab1_B.py

--- Lab1_B.py
+++ Lab1_B.py
@@ -1,3 +1,5 @@
+#Calculates the total cost of an item with sales tax
+
 price = float(input("Enter the price of the item: "))
 tax = float(input("Enter the sales tax percentage: "))
 total = (price + (price * tax) /100)
